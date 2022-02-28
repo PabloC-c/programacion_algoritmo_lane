3ef1bed7b36d4703865648dd810d62ae60a6a782
Gary Vidal Castillo <gary.vidal.c@gmail.com>
2022-02-28 18:39:55 -0300

    changes to start. Removed Comando_sbatch

--- start.py
+++ start.py
@@ -1,90 +1,32 @@
 import os
-import fileinput
 import sys
 
-## Funcion para reemplazar email
+archivo_prob = sys.argv[1]
+archivo_block = sys.argv[2]
+soluciones = sys.argv[3]
+restriccion = sys.argv[4]
+parada = sys.argv[5]
+modelo = sys.argv[6]
+cluster = False
 
-def replaceAll(file,searchExp,replaceExp):
-    for line in fileinput.input(file, inplace=1):
-        if searchExp in line:
-            line = line.replace(line,replaceExp)
-        sys.stdout.write(line)
-        
-##    
+if restriccion == "Libre":
+    restriccion = int(0)
+else:
+    restriccion = int(1)
 
-# Minas en numeral
-minas = { "kd" : "0", "chaiten" : "1"
-                     ,"marvin" : "2", "palomo" : "3"}
-# Tipos de modelo       
-tipo_de_modelo ={"Libre" : " 0", "Restringido":" 1"}
-
-# Paradas
-tipo_de_parada = {"Concava":" 0","Cauchy":" 1"}     
-                                            
-print("Insertar nombre de la mina :")
-nombre_mina = input()
-
-print("Utiliza cluster (Si/No)")
-utilizar_cluster = input()
-
-if utilizar_cluster == "Si":
-    print("Insertar email :")
-    mail = input()    
+if parada == "Concava":
+    parada = int(0)
+else:
+    parada = int(1) 
+     
+if modelo  == "Ordenado":
+    print("ord")
+    extension = ""
+else:
+    print("gen")
+    extension = "_alt"   
     
-print("Precedencias (Ordenadas/Generales)")
-precedencias = input()
-
-print("Modelo (Libre/Restringido)")
-modelo = input()
-
-print("Parada (Concava/Cauchy)")
-parada = input()
-
-if utilizar_cluster == "No":
-    if precedencias == "Ordenadas":
-        cmd = "python programa.py " + minas[nombre_mina] + tipo_de_modelo[modelo] + tipo_de_parada[parada]
-        os.system(cmd)
-
-    elif precedencias == "Generales":
-        cmd = "python programa_alt.py " + minas[nombre_mina] + tipo_de_modelo[modelo] + tipo_de_parada[parada]
-        os.system(cmd)    
-    else :
-        print("error precedencias")
-
-elif utilizar_cluster == "Si":
-    cmd = "Comando_sbatch/"+parada+"/"+modelo
-    os.chdir(cmd)
-    mina = int(minas[nombre_mina][-1])+1
-    if precedencias == "Generales":
-        texto ="_alt"
-    else :
-        texto = ""
-    if modelo == "Libre":
-        archivo = "programa"+str(mina)+texto+"_free.sh"
-        cmd = "sbatch "+ archivo
-        replaceAll(archivo,"--mail-user","#SBATCH --mail-user="+mail+"\n")
-        print("comando",cmd)
-        os.system(cmd)
-    if modelo == "Restringido":
-        archivo = "programa"+str(mina)+texto+"_restr.sh"
-        cmd = "sbatch "+ archivo
-        replaceAll(archivo,"--mail-user","#SBATCH --mail-user="+mail+"\n")
-        print("comando",cmd)
-        os.system(cmd)            
-                   
-
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+    
+cmd = "python programa"+extension+".py "+ archivo_prob +" "+ archivo_block+" "+str(restriccion) +" "+ str(parada) +" "+ soluciones
+print(cmd)
+os.system(cmd)
